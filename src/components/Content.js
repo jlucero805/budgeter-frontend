993a74a382bc57bd27737a3f43ea88317d7a2074
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2021-06-27 01:22:36 -0700

    added logout

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import { useUser } from '../providers/UserProvider'
 import { service } from '../services/service';
 import { isThisWeek, isThisMonth, isToday, isThisHour, parseJSON } from 'date-fns';
+import { strings } from '../res/strings';
 
 const Content = () => {
 	const { token, setToken } = useUser();
 	const { purchases, setPurchases } = useUser();
 	const { types, setTypes } = useUser();
+	const {loggedIn, setLoggedIn} = useUser();
 
 	const [adder, setAdder] = useState(true);
 	// adder field
@@ -20,14 +22,14 @@ const Content = () => {
 	const itemChanger = e => { setItem(e.target.value); }
 	const costChanger = e => { setCost(e.target.value); }
 
-	// HARD CODING
-	const defaults = [
-		'Materialistic Desires',
-		'Food',
-		'Personal Development',
-		'Entertainment',
-		'Professional'
-	]
+	const logoutClicker = () => {
+		setToken('');
+		localStorage.setItem('token', '');
+		setPurchases([]);
+		setTypes([]);
+		resetFields();
+		setLoggedIn(false);
+	}
 
 	const saveClicker = async () => {
 		if (type === '' || item === '' || cost === '') alert('please fill out all fields');
@@ -63,42 +65,43 @@ const Content = () => {
 	return (
 		<div>
 			<div className="nav">navbar</div>
-			<h2>total: {purchases.reduce((acc, purchase) => acc + purchase.cost, 0)}</h2>
-			<button className="add-btn" onClick={addClicker}>add</button>
-			<button onClick={dataClicker}>show data</button>
+			<h2>{strings.TOTAL}: {purchases.reduce((acc, purchase) => acc + purchase.cost, 0).toFixed(2)}</h2>
+			<button className="add-btn" onClick={addClicker}>{strings.ADD}</button>
+			<button onClick={dataClicker}>{strings.SHOW_DATA}</button>
 			<div className={showData ? '' : 'hide'}>
-				<h4 className="nopad">monthly: {purchases.filter(purchase => isThisMonth(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0)}</h4>
-				<h4 className="nopad">weekly: {purchases.filter(purchase => isThisWeek(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0)}</h4>
-				<h4 className="nopad">daily: {purchases.filter(purchase => isToday(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0)}</h4>
-				<h4 className="nopad">hourly: {purchases.filter(purchase => isThisHour(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0)}</h4>
+				<h4 className="nopad">{strings.MONTHLY}: {purchases.filter(purchase => isThisMonth(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0).toFixed(2)}</h4>
+				<h4 className="nopad">{strings.WEEKLY}: {purchases.filter(purchase => isThisWeek(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0).toFixed(2)}</h4>
+				<h4 className="nopad">{strings.DAILY}: {purchases.filter(purchase => isToday(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0).toFixed(2)}</h4>
+				<h4 className="nopad">{strings.HOURLY}: {purchases.filter(purchase => isThisHour(parseJSON(purchase.date))).reduce((acc, purchase) => acc + purchase.cost, 0).toFixed(2)}</h4>
 				<p></p>
 				{types.length > 0 ? types.map(def => (
 					<h4 key={def} className="nopad">
-						{def} : {purchases.filter(purchase => purchase.type === def).reduce((acc, purchase) => acc + purchase.cost, 0)}
+						{def} : {purchases.filter(purchase => purchase.type === def).reduce((acc, purchase) => acc + purchase.cost, 0).toFixed(2)}
 					</h4>
 				)) : null}
 			</div>
 			{
 				adder ? <div className="add-menu">
-					<div className="input-field"><p>item: </p> <input onChange={e => itemChanger(e)} value={item}></input></div>
-					<div className="input-field"><p>cost: </p> <input type="number" onChange={e => costChanger(e)} value={cost}></input></div>
-					<div className="input-field"><p>type: </p>
+					<div className="input-field"><p>{strings.ITEM}: </p> <input onChange={e => itemChanger(e)} value={item}></input></div>
+					<div className="input-field"><p>{strings.COST}: </p> <input type="number" onChange={e => costChanger(e)} value={cost}></input></div>
+					<div className="input-field"><p>{strings.TYPE}: </p>
 						<select onChange={e => typeChanger(e)} value={type}>
 							{types.map(def => (
 								<option key={def}>{def}</option>
 							))}
 						</select>
 					</div>
-					<div><button onClick={saveClicker}>save</button></div>
+					<div><button onClick={saveClicker}>{strings.SAVE}</button></div>
 				</div> : null
 			}
 			{
 				purchases.map(purchase => (
 					<div key={purchase.id}>
-						{`$${purchase.cost} - ${purchase.item}`} <button onClick={() => purchaseDeleter(purchase.id)}>delete</button>
+						{`$${purchase.cost} - ${purchase.item}`} <button onClick={() => purchaseDeleter(purchase.id)}>{strings.DELETE}</button>
 					</div>
 				))
 			}
+			<button onClick={logoutClicker}>{strings.LOGOUT}</button>
 		</div >
 	)
 }
--- src/res/strings.js
+++ src/res/strings.js
@@ -1,4 +1,17 @@
 export const strings = {
 	USERNAME: "username",
-	PASSWORD: 'password'
+	PASSWORD: 'password',
+	TOTAL: 'total',
+	ADD: "add",
+	SHOW_DATA: "show data",
+	MONTHLY: "monthly",
+	DAILY: "daily",
+	WEEKLY: "weekly",
+	HOURLY: "hourly",
+	ITEM: "item",
+	COST: "cost",
+	TYPE: "type",
+	SAVE: "save",
+	DELETE: "delete",
+	LOGOUT: "logout"
 }
